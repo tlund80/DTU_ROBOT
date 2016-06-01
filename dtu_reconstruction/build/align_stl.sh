38c3fb90d01a202d764531dc5dbad5f30518b52c
Thomas Sølund <thso@tekonologisk.dk>
2016-06-01 17:22:57 +0200

    changes

---
+++ dtu_reconstruction/build/align_stl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#base_path="/media/thso/Dataset1/DTUData/scenes/scene_data/"
+base_path="/media/thso/Elements/scene_data/"
+scene_name=""
+
+for i in $(seq -f "%03g" $1 $2)
+do
+   scene_name="scene_$i" 
+   echo "execute: ./dtu_reconstruction -align $base_path$scene_name"/" -stl"
+   ./dtu_reconstruction -align $base_path$scene_name"/" -stl
+done
